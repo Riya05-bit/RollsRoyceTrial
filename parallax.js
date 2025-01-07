f87fba76d499959559f32b98a7c669d36bbc9d81
Riya Indoria <136188578+Riya05-bit@users.noreply.github.com>
2025-01-07 16:13:20 +0530

    Add files via upload

---
+++ parallax.js
@@ -0,0 +1,28 @@
+let p1=document.getElementById('pop1');
+let p2=document.getElementById('pop2');
+let p3=document.getElementById('pop3');
+let h1=document.getElementById('heading1');
+let h2=document.getElementById('heading2');
+let h3=document.getElementById('heading3');
+let b1=document.getElementById('btn');
+// let c1=document.getElementsById('pc');
+// let n=document.getElementById('nav');
+// pop=()=>{
+//    p1.setAttribute("style","background-image: url('https://media.istockphoto.com/id/1456741702/photo/rolls-royce-with-famous-winged-emblem-mascot.jpg?s=612x612&w=0&k=20&c=f1AJJK1TnU-p56lbQtnEJ7al5N0AXuVllU90srjoT6k='); background-repeat: no-repeat;background-size:cover;color=white;");
+//     h1.setAttribute("style","opacity:0;");
+// }
+// pop1=()=>{
+//    p2.setAttribute("style","background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzQGj5lhJO03f2wdcU69Z6DTbAnJM6NbtCyg&usqp=CAU'); background-repeat: no-repeat;background-size:cover;color=white;");
+//    h2.setAttribute("style","opacity:0;");
+// }
+// pop2=()=>{
+//    p3.setAttribute("style","background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzQGj5lhJO03f2wdcU69Z6DTbAnJM6NbtCyg&usqp=CAU'); background-repeat: no-repeat;background-size:cover;color=white;");
+//    h3.setAttribute("style","opacity:0;");
+// }
+// scr=()=>{
+//     .setAttribute("style","opacity:0;");
+//     h1.setAttribute("style","opacity:0;");
+// }
+clickbtn=()=>{
+   b1.setAttribute("style","color:blue;background-color:red;");
+}
